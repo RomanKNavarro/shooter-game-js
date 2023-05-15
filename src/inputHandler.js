424c95218f5396103bb48d52f8873ef8885cb6fe
roman <ronnoverro@gmail.com>
2023-05-14 23:48:57 -0700

    fixed angle key issue

--- src/inputHandler.js
+++ src/inputHandler.js
@@ -1,14 +1,20 @@
 // keyboard keys
+
+// TODO: while shooting straight, pressing d+w makes bullets shoot up instead of diagnal,
+// yet, pressing w+d does make it diagnal.    --DONE. Simply had to move if statement to bottom of cases.
+
 export default class InputHandler {
     constructor(entity) {
       let keys = {"space": false, "d": false, "w": false};
+
+      
       document.addEventListener("keydown", (event) => {
         // TODO: try using if statements instead.
 
+        console.log(keys);
+        // what this do? sets respective keys value to true.
         keys[event.key] = true;
-        //console.log(keys);
-
-        if (keys["w"] && keys["d"]) entity.angle = "diagnal";
+        //console.log(entity.angle);
         
         switch (event.key) {
           // this is just for SHOOTING, not look direction
@@ -21,7 +27,9 @@ export default class InputHandler {
             entity.angle = "up";          
             break;
         }
-        console.log(entity.shooting);
+
+        // if (keys["w"] && keys["d"] || keys["d"] && keys["w"]) entity.angle = "diagnal";
+       if (keys["d"] && keys["w"]) entity.angle = "diagnal";
       });
   
       document.addEventListener("keyup", (event) => {
